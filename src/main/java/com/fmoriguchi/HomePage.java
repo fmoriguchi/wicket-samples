35b156149d2d4499e2d6793949284fe0e10ec8de
fmoriguchi <fmoriguchi@gmail.com>
2014-06-11 00:36:51 -0300

    .

--- src/main/java/com/fmoriguchi/HomePage.java
+++ src/main/java/com/fmoriguchi/HomePage.java
@@ -1,8 +1,12 @@
 package com.fmoriguchi;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import com.fmoriguchi.wicket.components.message.Alert;
+import com.fmoriguchi.wicket.components.message.Alert.Type;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -13,6 +17,6 @@ public class HomePage extends WebPage {
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
 		// TODO Add your page's components here
-
+        add(new Alert("alert", Model.of(getApplication().getFrameworkSettings().getVersion()), Type.INFO));
     }
 }
